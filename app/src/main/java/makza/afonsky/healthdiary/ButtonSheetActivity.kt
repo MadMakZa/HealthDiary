61780e442a09056b0ab81aa0d65c30100ee1a18b
MadMakZa <immakza@gmail.com>
2021-04-30 19:20:14 +0300

    bottomsheet test

--- app/src/main/java/makza/afonsky/healthdiary/ButtonSheetActivity.kt
+++
@@ -1,7 +0,0 @@
-package makza.afonsky.healthdiary
-
-import androidx.appcompat.app.AppCompatActivity
-
-class ButtonSheetActivity : AppCompatActivity() {
-
-}
--- app/src/main/java/makza/afonsky/healthdiary/MainActivity.kt
+++ app/src/main/java/makza/afonsky/healthdiary/MainActivity.kt
@@ -2,10 +2,21 @@ package makza.afonsky.healthdiary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import makza.afonsky.healthdiary.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var bindingClass: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindingClass = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
+
+        val bottomSheetFragment = BottomSheetFragment()
+
+        bindingClass.btnShowSheet.setOnClickListener {
+            bottomSheetFragment.show(supportFragmentManager, "ButtonSheetDialog")
+        }
     }
 }
