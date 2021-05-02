2ba835d236cfbc18e4cc9644deaf183d92430a11
MadMakZa <immakza@gmail.com>
2021-05-02 14:41:00 +0300

    bottomsheet + button addNote test

--- app/src/main/java/makza/afonsky/healthdiary/BottomSheetFragment.kt
+++ app/src/main/java/makza/afonsky/healthdiary/BottomSheetFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.Toast
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
@@ -15,13 +16,22 @@ class BottomSheetFragment: BottomSheetDialogFragment(){
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.bottomsheet_fragment, container, false)
+        val rootView = inflater.inflate(R.layout.bottomsheet_fragment, container, false)
+        val btnAddNote: Button = rootView.findViewById(R.id.btn_addNote)
+        btnAddNote.setOnClickListener {
+            Toast.makeText(activity, "Add note clicked!", Toast.LENGTH_SHORT).show()
+        }
+
+        return rootView
     }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
 
+
     }
+
 
 }
