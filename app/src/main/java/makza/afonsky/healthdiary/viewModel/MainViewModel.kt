e592b87e795fa5533d5d6f525ba1a756cabae3f3
MadMakZa <immakza@gmail.com>
2021-05-06 23:52:43 +0300

    add fab

--- app/src/main/java/makza/afonsky/healthdiary/viewModel/MainViewModel.kt
+++ app/src/main/java/makza/afonsky/healthdiary/viewModel/MainViewModel.kt
@@ -1,14 +1,13 @@
 package makza.afonsky.healthdiary.viewModel
 
 
-import android.util.Log
+
 import androidx.lifecycle.ViewModel
 
 
 class MainViewModel: ViewModel() {
 
     fun onClickAddNote(){
-        Log.d("test", "clicked Add NOTE")
         println("NOTE ADDED!!!!!!!!!!!!!")
     }
 
