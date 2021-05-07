b7bd69c4b4541475561db4f9248598fb826e5081
MadMakZa <immakza@gmail.com>
2021-05-07 15:56:04 +0300

    delete navigation bottom, refactor

--- app/src/main/java/makza/afonsky/healthdiary/view/NoteActivity.kt
+++ app/src/main/java/makza/afonsky/healthdiary/view/NoteActivity.kt
@@ -1,13 +1,8 @@
 package makza.afonsky.healthdiary.view
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import makza.afonsky.healthdiary.view.databinding.ActivityNoteBinding
+import makza.afonsky.healthdiary.databinding.ActivityNoteBinding
 
 class NoteActivity : AppCompatActivity() {
 
@@ -16,20 +11,7 @@ class NoteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityNoteBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_note)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
     }
 }
---
+++ app/src/main/java/makza/afonsky/healthdiary/viewModel/NoteViewModel.kt
@@ -0,0 +1,5 @@
+package makza.afonsky.healthdiary.viewModel
+
+class NoteViewModel: MainViewModel() {
+
+}
