a1913c6ef2d3ecd9252275a96c61b77023ea6d43
MadMakZa <immakza@gmail.com>
2021-05-27 23:41:27 +0300

    viewmodel for inser data

---
+++ app/src/main/java/makza/afonsky/healthdiary/model/db/ToDoRepository.kt
@@ -0,0 +1,12 @@
+package makza.afonsky.healthdiary.model.db
+
+import androidx.lifecycle.LiveData
+
+class ToDoRepository(private val toDoDao: ToDoDao) {
+
+    val getAllData: LiveData<List<ToDoData>> = toDoDao.getAllData()
+
+    suspend fun insertData(toDoData: ToDoData){
+        toDoDao.insertData(toDoData)
+    }
+}
---
+++ app/src/main/java/makza/afonsky/healthdiary/viewModel/ToDoViewModel.kt
@@ -0,0 +1,31 @@
+package makza.afonsky.healthdiary.viewModel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import makza.afonsky.healthdiary.model.db.ToDoData
+import makza.afonsky.healthdiary.model.db.ToDoDatabase
+import makza.afonsky.healthdiary.model.db.ToDoRepository
+
+class ToDoViewModel(application: Application): AndroidViewModel(application) {
+
+    private val toDoDao = ToDoDatabase.getDatabase(application).toDoDao()
+    private val repository: ToDoRepository
+
+    private val getAllData: LiveData<List<ToDoData>>
+
+    init {
+        repository = ToDoRepository(toDoDao)
+        getAllData = repository.getAllData
+    }
+
+    fun insertData(toDoData: ToDoData){
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.insertData(toDoData)
+        }
+    }
+
+}
